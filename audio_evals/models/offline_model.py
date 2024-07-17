fc2900f6600c2183da7c4ec6358c7cf77d7e7d7c
shiqundong <shiqundong@modelbest.cn>
2024-07-17 18:14:35 +0800

    change asr

--- audio_evals/models/offline_model.py
+++ audio_evals/models/offline_model.py
@@ -11,11 +11,14 @@ logger = logging.getLogger(__name__)
 class OfflineModel(Model):
     def __init__(self, is_chat: bool, path: str):
         super().__init__(is_chat)
+        logger.debug('start load model from {}'.format(path))
         self.model = AutoModelForCausalLM.from_pretrained(
             path,
             device_map="auto",
             trust_remote_code=True,
         ).eval()
+        logger.debug('successfully load model from {}'.format(path))
+
         self.tokenizer = AutoTokenizer.from_pretrained(path, trust_remote_code=True)
 
     def _inference(self, prompt: PromptStruct, **kwargs):
