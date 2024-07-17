929a4d9fd2a44659ae6c0f3ec73c4e91ca5b93b2
shiqundong <shiqundong@modelbest.cn>
2024-07-17 21:11:19 +0800

    add pretrain model

--- audio_evals/models/offline_model.py
+++ audio_evals/models/offline_model.py
@@ -28,3 +28,28 @@ class OfflineModel(Model):
         generated_text, _ = self.model.chat(self.tokenizer, query=query, history=None, **kwargs)
         logger.debug(f"the output is {generated_text}")
         return generated_text
+
+
+class OfflinePretrainModel(Model):
+    def __init__(self, is_chat: bool, path: str):
+        super().__init__(False)
+        logger.debug('start load model from {}'.format(path))
+        self.model = AutoModelForCausalLM.from_pretrained(
+            path,
+            device_map="auto",
+            trust_remote_code=True,
+        ).eval()
+        logger.debug('successfully load model from {}'.format(path))
+
+        self.tokenizer = AutoTokenizer.from_pretrained(path, trust_remote_code=True)
+
+    def _inference(self, prompt: PromptStruct, **kwargs) -> str:
+        assert isinstance(prompt, str), "pretrain model only receive string"
+        logger.debug(f"the input is {prompt}")
+        audio_info = self.tokenizer.process_audio(prompt)
+        inputs = self.tokenizer(prompt, return_tensors='pt', audio_info=audio_info)
+        inputs = inputs.to(self.model.device)
+        pred = self.model.generate(**inputs, audio_info=audio_info)
+        response = self.tokenizer.decode(pred.cpu()[0], skip_special_tokens=False, audio_info=audio_info)
+        logger.debug(f"the output is {response}")
+        return response
