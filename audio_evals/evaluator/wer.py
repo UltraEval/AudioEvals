6b62275178c714eb64c96c3ded46601bbc404486
shiqundong <shiqundong@modelbest.cn>
2024-07-19 16:18:21 +0800

    add libri asr

--- audio_evals/evaluator/wer.py
+++ audio_evals/evaluator/wer.py
@@ -4,13 +4,22 @@ from audio_evals.evaluator.base import Evaluator
 
 
 class WER(Evaluator):
+    def __init__(self, ignore_case: bool = False):
+        self.ignore_case = ignore_case
 
-    def __call__(self, pred, label, **kwargs):
+    def __call__(self, pred: str, label: str, **kwargs):
+        pred, label = str(pred), str(label)
+        if self.ignore_case:
+            pred, label = pred.lower(), label.lower()
         return {'match': wer(label, pred)}
 
 
 class CER(Evaluator):
+    def __init__(self, ignore_case: bool = False):
+        self.ignore_case = ignore_case
 
-    def __call__(self, pred, label, **kwargs):
+    def __call__(self, pred: str, label: str, **kwargs):
+        pred, label = str(pred), str(label)
+        if self.ignore_case:
+            pred, label = pred.lower(), label.lower()
         return {'match': cer(label, pred)}
-
